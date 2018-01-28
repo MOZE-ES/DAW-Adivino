9040f6a7904a41dad46c2616e5073ae2355a8010
Mindaugas Ozeraitis <mindo@hotmail.es>
2018-01-28 11:50:01 +0100

    Añadimos comentarios JavaDoc

--- src/Aciertos.java
+++ src/Aciertos.java
@@ -1,7 +1,21 @@
 import java.util.Scanner;
 
+/**
+ * 
+ * @version 1.0
+ * @author Mindo
+ * El proyecto plantea un juego: adivinar el número que genera de manera aleatoria el programa. 
+ * 
+ */
 public class Aciertos {
 
+    /**
+     * Este metodo es el que genera un número y pide al usuario que lo acierte.
+     * @param n Es INT y es el número introducido por el usuario
+     * @param cont Es INT y es el contador de cuantas veces ha tenido que introducir el número hasta acertar cual es.
+     * @param x es INT y es el número generado por el programa, el número máximo sera 500
+     * @param args 
+     */
     public static void main(String[] args) {
         int n = 0,cont = 0;
         int x = (int) (500 *Math.random());
